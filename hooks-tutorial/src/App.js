2f0cee310e1a4663b99905038c8acbec3644191d
ilhoon <rival6821@naver.com>
2019-05-01 23:27:59 +0900

    hook study Finish

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Average from './Average';
+import UsePromiseSample from './UsePromiseSample';
 
 const App = () => {
-  return <Average />;
+  return <UsePromiseSample />;
 };
 
 export default App;
